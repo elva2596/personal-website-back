0a5eb4ba04e0c0f29adc82c031b078d627901b17
elva2596 <18642175656@163.com>
2022-11-28 00:03:08 +0800

    update: delete add image api

--- routes/qiniu.js
+++ routes/qiniu.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const verifyToken = require("../middlewares/index");
-const addImg = require("../controllers/qiniu");
 const getToken = require("../controllers/getToken");
-router.post("/admin/imgs",addImg);
 router.get("/token", verifyToken, getToken)
 module.exports = router;
